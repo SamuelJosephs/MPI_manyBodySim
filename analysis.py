a5fe9e415e61cf7c06c7b6accd8e942e1b6fd5cf
Samuel Josephs <smdjosephs@gmail.com>
2023-01-01 13:39:28 +0000

    Fixed bug calculating energy difference over course of simulation

--- analysis.py
+++ analysis.py
@@ -12,9 +12,9 @@ for i in db:
     arrTotalE.append(i[-1])
     arrTotalGPE.append(i[-2])
     arrTotalKE.append(i[-3])
+print(f"First energy input = {arrTotalE[0]}")
 arrTotalE = [i for i in map(lambda x: 100*(arrTotalE[0] - x)/arrTotalE[0],arrTotalE)] # This is dumb but I had already gone through the trouble of using map.
-
-energy_difference = abs(((arrTotalE[-1] - arrTotalE[0])/arrTotalE[0])) * 100
+energy_difference = arrTotalE[-1]
 print(f"Energy Difference = {energy_difference}%")
 plt.plot(arrTotalE)
 plt.xlabel("iteration(s)")
